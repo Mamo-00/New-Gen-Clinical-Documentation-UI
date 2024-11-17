185cf0f8c470f941e73b7c037d91385a9ef634db
Mamo-00 <mahmodahmed2000@gmail.com>
2024-11-17 03:23:45 +0100

    setup the folder structure for the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { ThemeProvider, CssBaseline, useColorScheme, IconButton, Box } from "@mui/material";
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
+import { CssBaseline, useColorScheme, IconButton, Box } from "@mui/material";
+import { ThemeProvider } from '@mui/material/styles'
 import { theme } from "./theme";
 import EditorComponent from "./components/EditorComponent";
 import { Brightness4, Brightness7 } from "@mui/icons-material";
@@ -30,20 +30,18 @@ const ColorModeToggle = () => {
 
 const App: React.FC = () => {
   return (
-    <CssVarsProvider theme={theme}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Box sx={{ 
-          width: '100%', 
-          height: '100vh', 
-          backgroundColor: 'background.default',
-          position: 'relative'
-        }}>
-          <ColorModeToggle />
-          <EditorComponent />
-        </Box>
-      </ThemeProvider>
-    </CssVarsProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{ 
+        width: '100%', 
+        height: '100vh', 
+        backgroundColor: 'background.default',
+        position: 'relative'
+      }}>
+        <ColorModeToggle />
+        <EditorComponent />
+      </Box>
+    </ThemeProvider>
   );
 };
 
--- src/utils/mockMedicalConstants.ts
+++ src/utils/mockMedicalConstants.ts
@@ -1,42 +1,56 @@
-export const mockMedicalTerms = [
-  "abdomen",
-  "acute",
-  "anemia",
-  "angina",
-  "artery",
-  "asthma",
-  "blood pressure",
-  "bronchitis",
-  "cardiac",
-  "cholesterol",
-  "chronic",
-  "diabetes",
-  "diagnosis",
-  "diastolic",
-  "dyspnea",
-  "echocardiogram",
-  "edema",
-  "electrocardiogram",
-  "endocrine",
-  "gastrointestinal",
-  "hypertension",
-  "hypotension",
-  "intravenous",
-  "lymphatic",
-  "myocardial infarction",
-  "nausea",
-  "neurological",
-  "oncology",
-  "orthopedic",
-  "osteoporosis",
-  "palpitations",
-  "pediatric",
-  "prognosis",
-  "pulmonary",
-  "renal",
-  "rheumatoid",
-  "systolic",
-  "tachycardia",
-  "thyroid",
-  "vascular"
+import * as monaco from 'monaco-editor';
+
+export const conditionTerms = [
+  "diabetes", "hypertension", "asthma", "anemia", "chronic", "cardiac", 
+  "gastrointestinal", "rheumatoid", "vascular", "tachycardia", "osteoporosis",
+  "bronchitis", "lymphatic", "neurological", "myocardial infarction", 
+  "renal", "hypotension"
 ];
+
+export const procedureTerms = [
+  "blood test", "MRI scan", "X-ray", "surgery", "echocardiogram", 
+  "electrocardiogram", "palpitations", "diagnosis", "prognosis"
+];
+
+export const medicationTerms = [
+  "paracetamol", "ibuprofen", "metformin", "insulin", "cholesterol"
+];
+
+export const shorthandTerms = [
+  "BP", "HR", "RR", "SpO2"
+];
+
+export const complexMedicalTerms = [
+  {
+    label: "insulin",
+    insertText: "insulin (dose: ${1:10} IU)",
+    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
+  },
+  {
+    label: "insulin",
+    insertText: "insulin (dose: 10 IU)",
+    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
+  },
+  {
+    label: "insulin",
+    insertText: "insulin (dose: 20 IU)",
+    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
+  },
+  {
+    label: "BP",
+    insertText: "BP (Blood Pressure: ${1:120/80})",
+    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
+  },
+  {
+    label: "MRI Scan",
+    insertText: "MRI scan scheduled on ${1:date}",
+    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
+  },
+  {
+    label: "Paracetamol",
+    insertText: "Paracetamol 500mg",
+    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
+  }
+];
+
+export const bracketKeywords = ["critical", "warning", "info"];
